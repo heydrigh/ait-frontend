0b05822785822ada28a4cda38d4ca54d955efe0c
Heydrigh Ribeiro <heydrigh@gmail.com>
2025-01-17 13:35:27 -0300

    feat: add initial boilerplate

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,26 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+	baseDirectory: __dirname,
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+	...compat.config({
+		extends: [
+			'next',
+			'next/core-web-vitals',
+			'next/typescript',
+			'prettier',
+			'eslint:recommended',
+			'plugin:storybook/recommended',
+		],
+		rules: {},
+		plugins: ['jest'],
+	}),
+]
+export default eslintConfig
---
+++ jest/next-navigation-mock.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+
+jest.mock('next/navigation', () => ({
+	...require('next-router-mock'),
+	useRouter: jest.fn().mockReturnValue({
+		push: jest.fn(),
+	}),
+	usePathname: jest.fn(),
+}))
